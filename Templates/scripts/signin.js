f33ab3556dc03aadb451f23a9e821924600eb87b
kaburamew <kaburamew@gmail.com>
2020-03-10 20:10:43 +0200

    feat(signIn): create the signIn feature
       - unhide the signIn form
        [Finishes #171627586]

---
+++ Templates/scripts/signin.js
@@ -0,0 +1,10 @@
+function unhide() {
+    var x = document.getElementById("login-form");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
