770ce1b3ef88cc188add883c110f927822e5daad
kaburamew <kaburamew@gmail.com>
2020-03-11 14:39:01 +0200

    feat(signIn): create the signIn feature   - modify some components    [Finishes #171627586]

--- Templates/scripts/signin.js
+++ Templates/scripts/signin.js
@@ -1,10 +1,8 @@
 function unhide() {
-    var x = document.getElementById("login-form");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "block";
-    }
+  var form = document.getElementById("login-form");
+  if (form.style.display === "none") {
+    form.style.display = "none";
+  } else {
+    form.style.display = "block";
   }
-
-
+}
